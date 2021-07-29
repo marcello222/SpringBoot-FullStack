8420ae96b66f7f45a78f6bdc25d5d232b84732ca
Marcello <marcelloeduardo222@gmail.com>
2021-07-29 09:15:48 -0300

    Entity

--- src/main/java/com/marcello/cursomc/domain/Categoria.java
+++ src/main/java/com/marcello/cursomc/domain/Categoria.java
@@ -3,13 +3,21 @@ package com.marcello.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
+@Entity
+@Table(name= "tb_Categoria")
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
